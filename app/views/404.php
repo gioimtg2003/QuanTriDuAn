b007f15fda4fdcb30766a76c2203922f6f15a308
gioimtg2003 <conggioi.pro264@gmail.com>
2023-09-27 07:32:50 +0700

    update file 404

---
+++ app/views/404.php
@@ -0,0 +1,16 @@
+<?php if (ob_get_level() > 0) {
+    ob_clean();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error</title>
+</head>
+<body>
+    <h1>Link không tồn tại</h1>
+</body>
+</html>
