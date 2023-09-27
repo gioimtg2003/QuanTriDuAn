bc32b76b5629c226a55bdc788171d6f0656194bc
gioimtg2003 <conggioi.pro264@gmail.com>
2023-09-27 07:29:50 +0700

    update bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__ . '/configs/config.php';
+require_once __DIR__ . '/app/controllers/baseController.php';
+require_once __DIR__ . '/inc/header.php';
+require_once __DIR__ . '/app/models/connectDB.php';
+require_once __DIR__ . '/app/App.php';
+require_once __DIR__ . '/inc/footer.php';
+?>
