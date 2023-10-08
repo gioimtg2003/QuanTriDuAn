7e41f2f785060bf913933b9a6e3a9ff233c90fb4
gioimtg2003 <conggioi.pro264@gmail.com>
2023-10-08 22:16:05 +0700

    update

--- app/models/loginModel.php
+++ app/models/loginModel.php
@@ -4,10 +4,6 @@ class loginModel extends connectDB{
     public function __construct(){
         $this->connect = $this->connect();
     }
-    public function post($userName, $password){
-        $sql = "SELECT * FROM account WHERE userName = '$userName' AND password = '$password'";
-        $result = $this->connect->query($sql);
-        return $result->num_rows === 1 ? $result : false;
-    }
+    
 }
  ?>
